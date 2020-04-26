0989ce7e7e34afd5132220d43e5ee5912c09a198
FearVortex <47335510+FearVortex@users.noreply.github.com>
2020-04-26 16:37:02 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const {Client, RichEmbed} = require('discord.js');
+
+const bot = new Client();
+
+const ping = require('minecraft-server-util');
+
+const token ='NzAzODAwMzA4NDY3NDk5MTU4.XqXFpw.vdDpkDv6HWDKeE35NOgSS0oLirg';
+
+const PREFIX = '$';
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+})
+
+bot.on('message', msg => {
+  
+let args = Message.content.client.substring(PREFIX.lenght).split(' ');
+  
+  switch(args[0]){
+    case 'play':
+      ping('v4725.mineserver.io', 4725, (error, reponse)=>{
+        if (error) throw error
+      
+    const Embed =new RichEmbed()
+        .setTitle('Server Satus')
+        .addField('Server IP' ,reponse.host)
+        .addfield('Server Version' , reponse.version)
+        .addField('Online Players' , reponse.onlinePlayers)
+        .addField('Max Players' , reponse.maxPlayers)
+          message.channel.send(Embed)
+      })
+          break
+
+
+      
+  }
+  bot.login(process.env.BOT_TOKEN);
+})
